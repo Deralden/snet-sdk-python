ed12aeb829d49bd2c435524fb5e006ab03a962bc
Deralden <121487413+Deralden@users.noreply.github.com>
2024-06-21 12:02:59 +0300

    Update reset_environment.sh

--- testcases/utils/reset_environment.sh
+++ testcases/utils/reset_environment.sh
@@ -51,8 +51,8 @@ snet set current_multipartyescrow_at 0x5c7a4290f6f8ff64c69eeffdfafc8644a4ec3a4e
 
 # Create First identity (snet-user = first ganache).
 # (snet will automatically swith to this new identity)
-snet identity create snet-user rpc --network local
-#snet identity create --private-key "$SNET_TEST_WALLET_PRIVATE_KEY" rpc key --network local
+#snet identity create snet-user rpc --network local
+snet identity create --private-key "$SNET_TEST_WALLET_PRIVATE_KEY" rpc key --network local
 cat ~/.snet/config
 #sed -i "s/$FORMER_SNET_TEST_INFURA_KEY/$SNET_TEST_INFURA_KEY/g" ~/.snet/config
 export PYTHONPATH=$cwd
